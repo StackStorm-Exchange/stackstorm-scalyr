5c80c5fcc2961bf44e95835a4332e977fa2253d2
Dmitri Zimine <dz@softwarefrontier.com>
2019-07-18 14:54:44 -0700

    Minimal viable Scalyr pack.

--- actions/query.py
+++ actions/query.py
@@ -1,4 +1,6 @@
 from st2common.runners.base_action import Action
+import requests
+import json
 
 __all__ = ['QueryAction']
 
@@ -8,9 +10,17 @@ class QueryAction(Action):
 
         self.logger.info("Called with {0}".format(kwargs))
 
-        # Call Scalyr API here.
+        url = self.config['url'] + "/query"
+        kwargs['queryType'] = 'log'
+        if kwargs['token'] is None:
+            kwargs['token'] = self.config['token']
+        if not kwargs['filter']:
+            kwargs['filter'] = ""
 
-        results = ["line1", "line2"]
+        results = requests.request('POST', url=url, data=json.dumps(kwargs))
 
-        self.logger.info("Fetched {0}".format(results))
-        return (True, results)
+        ok = results.ok
+        self.logger.debug("Request {}, fetched {}".format("OK" if ok else "failed", results.text))
+        results = results.json() if ok else results.text
+        self.logger.info("Fetched {}".format(results))
+        return (ok, results)
