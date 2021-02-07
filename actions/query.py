122a65502fbb07b7d9ca442c90daa9bd46cd6f15
Tomaz Muraus <tomaz@tomaz.me>
2021-02-07 18:44:37 +0100

    Fix and update scalyr.query action:
    - Add missing header value without which the action doesn't work
    - Fix up various API parameter names
    - Add new columns parameter
    - Clean up the action code

--- actions/query.py
+++ actions/query.py
@@ -1,23 +1,42 @@
-from st2common.runners.base_action import Action
-import requests
 import json
+import copy
+
+import requests
+
+from st2common.runners.base_action import Action
 
 __all__ = ['QueryAction']
 
 
 class QueryAction(Action):
-    def run(self, **kwargs):
+    def run(self, filter="", maxCount=100, startTime=None, endTime=None, columns=None,
+            priority="low", token=None):
+        data = {
+            "queryType": "log",
+            "filter": filter,
+            "maxCount": maxCount,
+        }
 
-        self.logger.info("Called with {0}".format(kwargs))
+        if startTime:
+            data["startTime"] = startTime
+
+        if endTime:
+            data["endTime"] = startTime
+
+        if not token:
+            data["token"] = self.config["token"]
+
+        kwargs_to_log = copy.copy(data)
+
+        if "token" in kwargs_to_log:
+            kwargs_to_log["token"] = "************"
+
+        self.logger.info("Called with {0}".format(kwargs_to_log))
 
         url = self.config['url'] + "/query"
-        kwargs['queryType'] = 'log'
-        if kwargs['token'] is None:
-            kwargs['token'] = self.config['token']
-        if not kwargs['filter']:
-            kwargs['filter'] = ""
 
-        results = requests.request('POST', url=url, data=json.dumps(kwargs))
+        headers = {"Content-Type": "application/json"}
+        results = requests.request('POST', url=url, headers=headers, data=json.dumps(data))
 
         ok = results.ok
         self.logger.debug("Request {}, fetched {}".format("OK" if ok else "failed", results.text))
